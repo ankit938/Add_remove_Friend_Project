c2c1fa22e803c8dafec98991d1bfa13e43c204b3
ANKIT MISHRA <ankitmishra1022002@gmail.com>
2025-01-30 19:34:09 +0530

    Heart-Like

--- script.js
+++ script.js
@@ -6,13 +6,30 @@ addFriend.addEventListener("click",function(){
     if(flag==0){
     friend.innerHTML="Friends"
     friend.style.color="green"
+    addFriend.style.backgroundColor="red"
     addFriend.innerHTML="Remove Friend"
     flag=1
     }
     else{
         friend.innerHTML="Stranger"
         friend.style.color="red"
+        addFriend.style.backgroundColor="green"
         addFriend.innerHTML="Add Friend"
         flag=0
     }
 })
+
+var heart=document.querySelector(".ri-heart-3-fill")
+var temp=0
+heart.addEventListener("click",function(){
+if(temp==0){
+    heart.style.color="red"
+    temp=1
+}
+else{
+    heart.style.color="black"
+    temp=0
+}
+})
+
+
