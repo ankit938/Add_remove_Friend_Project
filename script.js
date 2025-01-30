e6b5cf8b0b2b83e6d736814af81a096804c279b4
ANKIT MISHRA <ankitmishra1022002@gmail.com>
2025-01-30 19:40:38 +0530

    Heart-Changes

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ var friend=document.querySelector("h5")
 var addFriend=document.querySelector("#add")
 var flag=0
 
+var heart=document.querySelector(".ri-heart-3-fill")
+//var temp=0
+
 addFriend.addEventListener("click",function(){
     if(flag==0){
     friend.innerHTML="Friends"
     friend.style.color="green"
     addFriend.style.backgroundColor="red"
     addFriend.innerHTML="Remove Friend"
+    heart.style.color="green"
     flag=1
     }
     else{
@@ -15,21 +19,21 @@ addFriend.addEventListener("click",function(){
         friend.style.color="red"
         addFriend.style.backgroundColor="green"
         addFriend.innerHTML="Add Friend"
+         heart.style.color="black"
         flag=0
     }
 })
 
-var heart=document.querySelector(".ri-heart-3-fill")
-var temp=0
-heart.addEventListener("click",function(){
-if(temp==0){
-    heart.style.color="red"
-    temp=1
-}
-else{
-    heart.style.color="black"
-    temp=0
-}
-})
+
+// heart.addEventListener("click",function(){
+// if(temp==0){
+//     heart.style.color="red"
+//     temp=1
+// }
+// else{
+//     heart.style.color="black"
+//     temp=0
+// }
+// })
 
 
